d83c3cfb24d42e282ab75ac977d7877e03fd5779
Paul H le <lepaulhz@gmail.com>
2024-05-27 22:05:58 -0700

    Clean up train_model and explored different models and looping through all previous datasets. Streamlined experiments

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,143 +1,183 @@
 # Import necessary libraries
 import pandas as pd
+from sklearn.model_selection import cross_val_score
 import os
 from datetime import datetime
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix, roc_auc_score
 from src.models.get_matrix import decipher_confusion_matrix
 from src.parameters.get_params import extract_values
-from src.logging.record_metrics_to_csv import record_metrics_to_csv
-from src.logging.remove_row_from_csv import remove_row_from_csv
-from src.logging.clear_csv_file import clear_csv_file
 from datetime import datetime
 import src.utilities.data_management as manage
-import yaml
+from src.visualization.plot_setting import setup_plotting, save_plot
+import src.visualization.visualization_utilities as visutil
+from sklearn.metrics import mean_absolute_error, mean_squared_error 
+
 
 # ------------------------------------------------------
 # Load Data
 # ------------------------------------------------------
-
+result_df = pd.DataFrame()
 load_params_dir = '../../src/parameters/'
-load_base_filename_params = 'master_params_df'
 load_file_extension_params = '.yaml'
-load_file_number_params = 25
-master_params = manage.load_from_file(load_params_dir, load_base_filename_params, load_file_extension_params, load_file_number_params)
+_, max_num = manage.get_file_list_and_max_number(load_params_dir, load_file_extension_params)
+
+for file_number in range(1, max_num+1):
+    load_base_filename_params = 'master_params_df'
+    load_file_number_params = file_number
+    master_params = manage.load_from_file(load_params_dir, load_base_filename_params, load_file_extension_params, load_file_number_params)
 
 
-load_processed_dir = '../../data/processed/'
-load_base_filenames_processed = ['X_test_processed_df', 
-                                 'X_train_processed_df', 
-                                 'y_test_processed_df', 
-                                 'y_train_processed_df']
-load_file_extension_processed = '.pkl'
-load_file_number_processed = 25
-X_test = manage.load_from_file(load_processed_dir, load_base_filenames_processed[0], load_file_extension_processed, load_file_number_processed)
-X_train = manage.load_from_file(load_processed_dir, load_base_filenames_processed[1], load_file_extension_processed, load_file_number_processed)
-y_test = manage.load_from_file(load_processed_dir, load_base_filenames_processed[2], load_file_extension_processed, load_file_number_processed)
-y_train = manage.load_from_file(load_processed_dir, load_base_filenames_processed[3], load_file_extension_processed, load_file_number_processed)
+    load_processed_dir = '../../data/processed/'
+    load_base_filenames_processed = ['X_test_processed_df', 
+                                    'X_train_processed_df', 
+                                    'y_test_processed_df', 
+                                    'y_train_processed_df']
+    load_file_extension_processed = '.pkl'
+    load_file_number_processed = file_number
+    X_test = manage.load_from_file(load_processed_dir, load_base_filenames_processed[0], load_file_extension_processed, load_file_number_processed)
+    X_train = manage.load_from_file(load_processed_dir, load_base_filenames_processed[1], load_file_extension_processed, load_file_number_processed)
+    y_test = manage.load_from_file(load_processed_dir, load_base_filenames_processed[2], load_file_extension_processed, load_file_number_processed)
+    y_train = manage.load_from_file(load_processed_dir, load_base_filenames_processed[3], load_file_extension_processed, load_file_number_processed)
+    
+    # Ensure y_train is a 1-dimensional array
+    y_train = y_train.values.ravel()
+    y_test = y_test.values.ravel()
 
-# ------------------------------------------------------
-# Model
-# ------------------------------------------------------
+    # Convert X_train and X_test to dense format explicitly
+    X_train = X_train.to_numpy()
+    X_test = X_test.to_numpy()    
+    
+    # ------------------------------------------------------
+    # Model
+    # ------------------------------------------------------
+    
+    # Define the hyperparameter grids for each model
+    logreq_hyperparams = dict(max_iter = 1000)
+    svm_hyperparams = dict(kernel = 'linear') 
+    rf_hyperparams = dict(random_state = 42)
 
-# Define hyperparameters for Logistic Regression model
-logreq_hyperparams = dict(max_iter = 1000)
+    # Define models
+    models = [(LogisticRegression(), logreq_hyperparams),
+              (SVC(), svm_hyperparams),
+              (RandomForestClassifier(), rf_hyperparams)]
+    
+    for model, model_hyperparams in models:
+        # Initialize model with hyperparameters
+        model.set_params(**model_hyperparams)
+        
+        # Add model & model_hyperparameters to master_params
+        master_params['model'] = model.__class__.__name__
+        master_params['model_hyperparams'] = model_hyperparams
+        
+        model.fit(X_train, y_train)
 
-# Initialize Logistic Regression model
-model = LogisticRegression(**logreq_hyperparams)
+        # Add time_stamp to master_params
+        time_stamp = dict(Timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+        master_params['time_stamp'] = time_stamp['Timestamp']
 
-# Add model & model_hyperparameters to master_params
-master_params['model'] = model.__class__.__name__
-master_params['model_hyperparameters'] = logreq_hyperparams
+        # Predictions
+        y_test_pred = model.predict(X_test)
+        y_train_pred = model.predict(X_train)
+        
+        # Calculate MSE & store the dictionary
+        mse_train = mean_squared_error(y_train, y_train_pred)
+        mse_test = mean_squared_error(y_test, y_test_pred)
+        mse_metrics = dict(mse_train = mse_train, mse_test = mse_test)
+        master_params['mse_metrics'] = mse_metrics
+        
+        # Calculate MAE & store the dictionary
+        mae_train = mean_absolute_error(y_train, y_train_pred)
+        mae_test = mean_absolute_error(y_test, y_test_pred)
+        mae_metrics = dict(mae_train = mae_train, mae_test = mae_test)
+        master_params['mae_metrics'] = mae_metrics
+        
+        # Calculate AUC scores & store the dictionary
+        auc_score_train = roc_auc_score(y_train, y_train_pred)
+        auc_score_test = roc_auc_score(y_test, y_test_pred)
+        auc_metrics = dict(auc_train = auc_score_train, auc_test = auc_score_test)
+        master_params['auc_metrics'] = auc_metrics
 
-# Add time_stamp to master_params
-time_stamp = dict(Timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-master_params['time_stamp'] = time_stamp['Timestamp']
+        # Confusion matrices
+        matrix_train = confusion_matrix(y_train_pred, y_train)
+        matrix_test = confusion_matrix(y_test_pred, y_test)
 
-# Fit the model
-model.fit(X_train, y_train.values.ravel())
+        # Calculate metrics from confusion matrices
+        metrics_train = decipher_confusion_matrix(matrix_train)
+        metrics_test = decipher_confusion_matrix(matrix_test)
 
-# Get the coefficients and intercept
-coefficients = model.coef_[0]
-intercept = model.intercept_[0]
-features = X_train.columns
+        # Store performance metrics in a dictionary
+        metrics_performance = dict(TN_train = metrics_train[0], TN_test = metrics_test[0],
+                                   FP_train = metrics_train[1], FP_test = metrics_test[1],
+                                   FN_train = metrics_train[2], FN_test = metrics_test[2],
+                                   TP_train = metrics_train[3], TP_test = metrics_test[3],
+                                   accuracy_train = metrics_train[4],accuracy_test = metrics_test[4],
+                                   precision_train = metrics_train[5], precision_test = metrics_test[5],
+                                   recall_train = metrics_train[6],recall_test = metrics_test[6],
+                                   F1_train = metrics_train[7],F1_test = metrics_test[7])
 
-# Create a DataFrame to store coefficients and intercept
-coefficients_df = pd.DataFrame({'Feature': features, 'Coefficient': coefficients, 'Intercept': intercept})
+        # Update master_params with performance metrics
+        master_params['metrics_performance'] = metrics_performance
 
-# Predictions
-y_test_pred = model.predict(X_test)
-y_train_pred = model.predict(X_train)
+        # Extract key-value pairs from master_params
+        metric_kv_pairs = extract_values(master_params)
 
-# Calculate AUC scores
-auc_score_train = roc_auc_score(y_train, y_train_pred)
-auc_score_test = roc_auc_score(y_test, y_test_pred)
+        # Convert the filtered dictionary to a DataFrame with one row
+        metrics_df = pd.DataFrame([metric_kv_pairs])
 
-# Store AUC metrics in a dictionary
-auc_metrics = dict(auc_train = auc_score_train, auc_test = auc_score_test)
-master_params['auc_metrics'] = auc_metrics
+        # Define the desired columns to move to the front and back
+        desired_front_columns = ['time_stamp','dataset_number', 'model', 
+                                'accuracy_train', 'accuracy_test', 
+                                'precision_train', 'precision_test', 
+                                'auc_train','auc_test']
 
-# Confusion matrices
-matrix_train = confusion_matrix(y_train_pred, y_train)
-matrix_test = confusion_matrix(y_test_pred, y_test)
+        desired_back_columns =  ['TN_train', 'TN_test',
+                                'FP_train', 'FP_test', 
+                                'FN_train', 'FN_test', 
+                                'TP_train', 'TP_test']
 
-# Calculate metrics from confusion matrices
-metrics_train = decipher_confusion_matrix(matrix_train)
-metrics_test = decipher_confusion_matrix(matrix_test)
+        # Extract the middle columns
+        desired_middle_columns = (metrics_df.drop(columns=(desired_front_columns + desired_back_columns))).columns.to_list()
 
-# Store performance metrics in a dictionary
-metrics_performance = dict(TN_train = metrics_train[0], TN_test = metrics_test[0],
-                           FP_train = metrics_train[1], FP_test = metrics_test[1],
-                           FN_train = metrics_train[2], FN_test = metrics_test[2],
-                           TP_train = metrics_train[3], TP_test = metrics_test[3],
-                           accuracy_train = metrics_train[4],accuracy_test = metrics_test[4],
-                           precision_train = metrics_train[5], precision_test = metrics_test[5],
-                           recall_train = metrics_train[6],recall_test = metrics_test[6],
-                           F1_train = metrics_train[7],F1_test = metrics_test[7])
+        # Reorganize the metrics DataFrame with desired column order
+        metrics_df = metrics_df[desired_front_columns + desired_middle_columns + desired_back_columns]
+        
+        # Debugging information for the final dataframe
+        result_df = pd.concat([result_df, metrics_df], ignore_index=True)
+        print(f'Data Frame: {file_number} Analysis Completed for {model.__class__.__name__}')
 
-# Update master_params with performance metrics
-master_params['metrics_performance'] = metrics_performance
+result_df.columns
+len(result_df.columns)
 
-# Extract key-value pairs from master_params
-metric_kv_pairs = extract_values(master_params)
+# # Get the coefficients and intercept
+# coefficients = model.coef_[0]
+# intercept = model.intercept_[0]
+# features = X_train.columns
 
-# Convert the filtered dictionary to a DataFrame with one row
-metrics_df = pd.DataFrame([metric_kv_pairs])
-
-# Define the desired columns to move to the front and back
-desired_front_columns = ['time_stamp','dataset_number', 'model', 
-                         'accuracy_train', 'accuracy_test', 
-                         'precision_train', 'precision_test', 
-                         'auc_train','auc_test']
-
-desired_back_columns =  ['TN_train', 'TN_test',
-                         'FP_train', 'FP_test', 
-                         'FN_train', 'FN_test', 
-                         'TP_train', 'TP_test']
-
-# Extract the middle columns
-desired_middle_columns = (metrics_df.drop(columns=(desired_front_columns + desired_back_columns))).columns.to_list()
-
-# Reorganize the metrics DataFrame with desired column order
-metrics_df = metrics_df[desired_front_columns + desired_middle_columns + desired_back_columns]
+# # Create a DataFrame to store coefficients and intercept
+# coefficients_df = pd.DataFrame({'Feature': features, 'Coefficient': coefficients, 'Intercept': intercept})
 
 # ------------------------------------------------------
 # Logging Metrics Copy
 # ------------------------------------------------------
 
-# Define file path
-csv_file_path = os.path.join('../../data/metrics', 'master_metric_log.csv')
-pickle_file_path = os.path.join('../../data/metrics', 'master_metric_log.pkl')
+# Define file paths
+csv_metrics_file_path = os.path.join('../../data/metrics', 'master_metric_log.csv')
+pickle_metrics_file_path = os.path.join('../../data/metrics', 'master_metric_log.pkl')
+# old_pickle_metrics_file_path = os.path.join('../../data/metrics', '6.0-master_metric_log.pkl')
 
 # Read the master metrics from source and make a copy
-master_metrics_df = pd.read_pickle(pickle_file_path)
-copy_master_metrics_df = master_metrics_df.copy()
-copy_master_metrics_df.T
+master_metrics_df = pd.read_pickle(pickle_metrics_file_path)
+copied_master_metrics_df = master_metrics_df.copy()
 
- 
-# Perform actions on copy version
-copy_master_metrics_df = pd.concat([copy_master_metrics_df,metrics_df], ignore_index=True)
-copy_master_metrics_df.iloc[-1].to_frame()
+
+## Perform actions on copied version
+# copied_master_metrics_df = pd.concat([copied_master_metrics_df, result_df], ignore_index=True)
+    
+setup_plotting()
+visutil.plot_model(copied_master_metrics_df)
 
 
 # ------------------------------------------------------
@@ -145,18 +185,7 @@ copy_master_metrics_df.iloc[-1].to_frame()
 # ------------------------------------------------------
 
 # Recording copy master metric to pickle file as master metrics
-# copy_master_metrics_df.to_pickle(pickle_file_path)
-# copy_master_metrics_df.to_csv(csv_file_path)
-
-# ------------------------------------------------------
-# Edit Logging Metrics csv
-# ------------------------------------------------------
-#record_metrics_to_csv(metrics_df, csv_file_path)
-
-# Remove a specfic row from csv
-#remove_row_from_csv.remove_row_from_csv(csv_file_path, #)
-
-# Clear the contents of the csv file
-#clear_csv_file.clear_csv_file(csv_file_path)
+# copied_master_metrics_df.to_pickle(pickle_metrics_file_path)
+# copied_master_metrics_df.to_csv(csv_metrics_file_path)
 
 
--- src/visualization/visualization_utilities.py
+++ src/visualization/visualization_utilities.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 def mission_percent(df, column_name):
     # Count total, success, and failure missions directly
     mission_count = df[column_name].value_counts().sort_index()
@@ -10,3 +13,54 @@ def mission_percent(df, column_name):
     
     return success_percent, failure_percent
 
+def plot_model(master_metrics):
+    for model in master_metrics['model'].unique():
+        # Filter for the current model
+        filtered_metrics = master_metrics[master_metrics['model'] == model]
+        
+        # Group by 'dataset_number' and find the index of the maximum 'auc_test' value for each group
+        idx_max_auc = filtered_metrics.groupby('dataset_number')['auc_test'].idxmax()
+        
+        # Use these indices to filter the DataFrame
+        filtered_max_auc = filtered_metrics.loc[idx_max_auc]
+        
+        # Reshaping the df from wide to long format
+        filtered_melted = filtered_max_auc.melt(id_vars=['dataset_number'], value_vars=['auc_train', 'auc_test'], var_name='AUC_Type', value_name='AUC_Value')
+        filtered_melted['AUC_Value'] = filtered_melted['AUC_Value'].astype(float).round(4)
+        
+        # Plot the results
+        ax = sns.barplot(data=filtered_melted, x='dataset_number', y='AUC_Value', hue='AUC_Type')
+        
+        # Find the maximum auc_test value
+        max_auc_test = filtered_max_auc['auc_test'].max()
+        max_row = filtered_max_auc[filtered_max_auc['auc_test'] == max_auc_test]
+        max_dataset_number = max_row['dataset_number']
+        
+        # Add a dot at the highest auc_test value
+        ax.plot(max_dataset_number-1, max_auc_test, 'ro')
+        
+        # ------------------------------------------------------
+        
+        # Find the index of the minimum 'mae_test' value for each group
+        idx_min_mae = filtered_metrics.groupby('dataset_number')['mae_test'].idxmin()
+        
+        # Use these indices to filter the DataFrame
+        filtered_min_mae = filtered_metrics.loc[idx_min_mae]
+        
+        # Find the minimum mae_test value
+        min_mae_test = filtered_min_mae['mae_test'].min()
+        
+        # Find the row with the minimum mae_test value
+        min_row = filtered_min_mae[filtered_min_mae['mae_test'] == min_mae_test]
+        
+        # Get the dataset number corresponding to the row with the minimum mae_test value
+        min_dataset_number = min_row['dataset_number']
+
+        # Add a dot at the lowest mae_test value
+        ax.plot(min_dataset_number-1, min_mae_test, 'bo')
+        
+        # ------------------------------------------------------
+        
+        plt.title(f'{model}')
+        plt.legend(loc='lower right')
+        plt.show()
